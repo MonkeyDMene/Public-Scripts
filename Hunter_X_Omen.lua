f8a92f70dd1024c0fd958ab160d3af636143b240
AisyIn <101306121+MonkeyDMene@users.noreply.github.com>
2022-05-11 10:34:51 -0400

    Update Hunter_X_Omen.lua

--- Hunter_X_Omen.lua
+++ Hunter_X_Omen.lua
@@ -2,12 +2,20 @@ task.spawn(function()
     while task.wait() do
         pcall(function()
                   if game:GetService("Workspace").Living[game.Players.LocalPlayer.Name].Humanoid.Info.Stamina.Value == 100 then
-                  local args = {
-    [1] = "J",
-    [2] = function()end
-}
+                  _G.Enabled = true
+local plrs = game:GetService("Players")
 
-game:GetService("Players").LocalPlayer.Character.Character.input:FireServer(unpack(args))
+game:GetService("RunService").Stepped:Connect(function()
+    if _G.Enabled then
+        if plrs.LocalPlayer.Character.Humanoid.Info.Stamina.Value >= 1 then
+            if not plrs.LocalPlayer.PlayerGui:FindFirstChild("PushupsGui") then
+                game:GetService("Players").LocalPlayer.Character.Character.input:FireServer("J", function()end)
+            else
+                plrs.LocalPlayer.PlayerGui.PushupsGui.Pushups.RemoteEvent:FireServer()
+            end
+        end
+    end
+end)
                   else 
                   if game:GetService("Workspace").Living[game.Players.LocalPlayer.Name].Humanoid.Info.Stamina.Value == 0 then
                   game.Players.LocalPlayer.Character:Destroy()
