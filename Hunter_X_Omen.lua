d118bf9b268e3311119b116772352c92ec0d4fc9
AisyIn <101306121+MonkeyDMene@users.noreply.github.com>
2022-05-12 17:03:28 -0400

    Update Hunter_X_Omen.lua

--- Hunter_X_Omen.lua
+++ Hunter_X_Omen.lua
@@ -1,8 +1,4 @@
-task.spawn(function()
-    while task.wait() do
-        pcall(function()
-                  if game:GetService("Workspace").Living[game.Players.LocalPlayer.Name].Humanoid.Info.Stamina.Value == 100 then
-                  _G.Enabled = true
+G.Enabled = true
 local plrs = game:GetService("Players")
 
 game:GetService("RunService").Stepped:Connect(function()
@@ -14,13 +10,5 @@ game:GetService("RunService").Stepped:Connect(function()
                 plrs.LocalPlayer.PlayerGui.PushupsGui.Pushups.RemoteEvent:FireServer()
             end
         end
-    end
-end)
-                  else 
-                  if game:GetService("Workspace").Living[game.Players.LocalPlayer.Name].Humanoid.Info.Stamina.Value == 0 then
-                  game.Players.LocalPlayer.Character:Destroy()
-               end
-            end
-        end)
     end
 end)
